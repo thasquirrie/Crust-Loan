6fbe03a304c37031043278470299b6fa9dce78a3
Blessed Dominic <blessed@crust.africa>
2023-04-30 05:38:30 +0000

    Feat/aggregator summary

---
+++ src/components/posAggregators/ViewAggregatorSummary.js
@@ -0,0 +1,373 @@
+import { forwardRef,useEffect, useState} from "react";
+import Dialog from "@mui/material/Dialog";
+import Slide from "@mui/material/Slide";
+import styled from "styled-components";
+import closeModalIcon from "../../assets/posRequests/close.svg";
+import {
+    useGetAggregatorAgentQuery,
+    useGetAggregatorTransactionQuery,
+    useGetAggregatorPosDevicesQuery,
+} from "../../app/services/pos";
+import Table from "../../components/common/Table"
+import Avatar from 'react-avatar';
+import { setAggregatorAgents,  resetAggregatorAgents, resetAggregatorPos, setAggregatorPos } from "../../features/pos/posSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+const Transition = forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const PosDevicesTableColumns = [
+    { id: "serialNumber", label: "SERIAL NO." },
+    { id: "terminalId", label: "TERMINAL NO." },
+    { id: "merchantName", label: "Merchant Name" },
+    { id: "agentName", label: "Agent Name" },
+    { id: "agentAccountNumber", label: "Agent Acc. No." },
+    { id: "posState", label: "POS State" },
+];
+
+const AggregatorAgentTableColumns = [
+    { id: "agentName", label: "Agent Name" },
+    { id: "email", label: "Email" },
+    { id: "phoneNumber", label: "Phone Number" },
+    { id: "dateAdded", label: "Date Added" },
+];
+
+export default function ViewAggregatorSummary({ open, handleClose, aggregatorDetails}) {
+    const [tab, setTab] = useState("posdevices");
+
+    const {   aggregatorPosDevices,
+        aggregatorAgents,
+        aggregatorTransactionCount } =  useSelector((state) => state.pos)
+    const dispatch = useDispatch();
+
+    const { data: aggregatorPosDeviceDetails, fulfilledTimeStamp: posDevicesTimestamp } = useGetAggregatorPosDevicesQuery(aggregatorDetails?.id, {
+        skip:!aggregatorDetails?.id
+    })
+  
+    const { data: lazyQueryAggregatorAgents, fulfilledTimeStamp: agentsTimestamp  } = useGetAggregatorAgentQuery(aggregatorDetails?.id, {
+        skip:!aggregatorDetails?.id
+    })
+
+
+    const 
+        { data: lazyQueryTransactionCount, fulfilledTimeStamp: transactionCountTimestamp 
+        } = useGetAggregatorTransactionQuery(aggregatorDetails?.id, {
+            skip:!aggregatorDetails?.id
+        })
+
+        useEffect(() => {
+         if(posDevicesTimestamp){
+           dispatch(setAggregatorPos(aggregatorPosDeviceDetails))
+         }
+        },[aggregatorPosDevices, dispatch, posDevicesTimestamp])
+
+
+        useEffect(() => {
+            if(agentsTimestamp ){
+              dispatch(setAggregatorAgents(lazyQueryAggregatorAgents))
+            }
+           },[lazyQueryAggregatorAgents, dispatch, agentsTimestamp])
+           
+
+            const hanldeCloseModal = () => {
+                handleClose()
+                setTab("posdevices")
+                dispatch(resetAggregatorPos())
+                dispatch( resetAggregatorAgents())
+            }
+    return (
+        <div>
+            <Dialog
+                maxWidth={"xl"}
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <ModalContainer>
+                  
+                        <>
+                            <ModalHeader>
+                                <AggregatorHeader>
+                                    <h4>
+                                        Aggregator Summary
+                                    </h4>
+
+                                    <img onClick={ hanldeCloseModal} src={closeModalIcon} alt='close modal' />
+                                </AggregatorHeader>
+
+                                <AggregatorInfo> 
+                                    <Avatar name={aggregatorDetails?.aggregatorName} round color='#933D0C' />
+                                   
+                                    <h4>{aggregatorDetails?.aggregatorName}</h4>
+
+                                    <ul className="aggregator-details">
+                                        <li>Account NO: <span>{aggregatorDetails?.aggregatorName}</span></li>
+                                        <li>Phone Number: <span>{aggregatorDetails?.aggregatorPhoneNumber}</span></li>
+                                        <li>State: <span>{aggregatorDetails?.aggregatorState}</span></li>
+                                    </ul>
+                                </AggregatorInfo>
+                                <PillsContainer>
+                                
+                                    <Pills
+                                        onClick={() => {
+                                            setTab("posdevices")
+                                        }}
+                                        active={tab === "posdevices"}
+                                    >
+                                        <p>POS Devices</p>
+                                    </Pills>
+                                  
+                                    <Pills
+                                        onClick={() => {
+                                            setTab("agents");
+                                        }}
+                                        active={tab === "agents"}
+                                    >
+                                        <p>Agents</p>
+                                    </Pills>
+                                  
+                                    <Pills
+                                        onClick={() => {
+                                            setTab("postransactions")
+                                        }}
+                                        active={tab === "postransactions"}
+                                    >
+                                        <p>POS Transactions</p>
+                                    </Pills>
+                                
+                                </PillsContainer>
+                            </ModalHeader>
+                            <ModalBody>
+                                {tab === "posdevices" &&  (
+                                    <>
+                                        <TableContainer>
+                                            {( aggregatorPosDevices) &&
+                                              <ListOfItems>
+                                                 {aggregatorPosDevices.length  } Assigned POS Devices
+                                            </ListOfItems>
+                                            }
+                                          
+                                            <Table
+                                            heightOfTable={"420px"}
+                                                columns={PosDevicesTableColumns}
+                                                rows={aggregatorPosDevices}
+                                            />
+                                        </TableContainer>
+
+                                    </>
+                                )}
+                                 {tab === "agents"  && (
+                                    <>
+                                        <TableContainer>
+                                        {  aggregatorAgents &&
+                                              <ListOfItems>
+                                                 {  aggregatorAgents?.length } Mapped Agents
+                                            </ListOfItems>
+                                            }
+                                          
+                                            <Table
+                                               heightOfTable={"420px"}
+                                                columns={AggregatorAgentTableColumns}
+                                                rows={aggregatorAgents}
+                                            />
+                                        </TableContainer>
+                                    </>
+                                )} 
+                                {tab === "postransactions" && (
+                                    <>
+                                                  <POSTransactionContainer>
+                                                    <POSTransactionCard>
+                                                        <div className="card-header">Total POS Transactions</div>
+                                                        <div className="card-amount">{lazyQueryTransactionCount?.data?.totalPosTransactions}</div>
+                                                    </POSTransactionCard>
+                                                    <POSTransactionCard>
+                                                        <div className="card-header">POS Transfer Count</div>
+                                                        <div className="card-amount">{lazyQueryTransactionCount?.data?.posTransferCount}</div>
+                                                    </POSTransactionCard>
+                                                    <POSTransactionCard>
+                                                        <div className="card-header">POS Withdrawal Count</div>
+                                                        <div className="card-amount">{lazyQueryTransactionCount?.data?.posWithdrawalCount}</div>
+                                                    </POSTransactionCard>
+                                                </POSTransactionContainer>
+                                    </>
+                                )}
+                            </ModalBody>
+                        </>
+                </ModalContainer>
+            </Dialog>
+        </div>
+    );
+}
+
+const ModalContainer = styled.div`
+    // width: 100%;
+    width: 72.0625rem;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 40px;
+    /* overflow: hidden; */
+`;
+
+const ModalHeader = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    background: #fff;
+    box-sizing: border-box;
+`;
+
+const AggregatorHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    background: #fff;
+    box-sizing: border-box;
+
+
+    h4 {
+        font-weight: 700;
+        font-size: 1.1rem;
+        line-height: 1.2rem;
+        color: black;
+    }
+
+    img {
+        cursor: pointer;
+    }
+`;
+
+const AggregatorInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    background: #fff;
+    margin-top: 37px;
+
+    h4 {
+        font-style: normal;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 12px;
+        color: #933D0C;
+        margin-top: 23px;
+    }
+
+    .aggregator-details {
+        display:flex;
+        margin-top: 29px;
+        flex-direction: row;
+        align-items: center;
+
+
+         li {
+            font-style: normal;
+            font-weight: 500;
+            font-size: 12px;
+            line-height: 4px;
+            display: flex;
+            margin-right: 16px;
+            align-items: center;
+            color: #292929;
+            span {
+                margin-left: 4px;
+            }
+        }
+    }
+`;
+
+const ModalBody = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow-y: auto;
+    scrollbar-width: thin;
+`;
+
+const PillsContainer = styled.div`
+    display: flex;
+    align-items: baseline;
+    width: auto;
+    justify-content: center;
+    margin: 1rem 0;
+
+    div:nth-child(2) {
+        margin: 0.9rem;
+    }
+`;
+
+const Pills = styled.div`
+    cursor: pointer;
+    padding: 7.5px 18px;
+    border: 1px solid #933d0c;
+    border-radius: 20px;
+    background: ${(props) => (props.active ? "#FFFAF6" : "#fff")};
+    border: ${(props) => (props.active ? "1px solid #933d0c" : "1px solid #7a7a7a")};
+
+    p {
+        color: ${(props) => (props.active ? "#933d0c" : "#0C0400")};
+    }
+`;
+
+const TableContainer = styled.div`
+  margin-top: 69px;
+  width: 100%;
+`
+
+const ListOfItems = styled.h2`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 16px;
+    color: #292929;
+    margin-bottom: 37px;
+`
+
+const POSTransactionContainer = styled.div`
+  display:flex;
+  align-items: center;
+  flex-direction: row;
+  margin-top:60px;
+`
+
+const POSTransactionCard = styled.div`
+    background: #FFFFFF;
+    border: 0.5px solid #D3D3D3;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.03);
+    border-radius: 6px;
+    padding-left:20px;
+    padding-top: 20px;
+    margin-right: 16px;
+    width: 337px;
+    height: 142px;
+    .card-header {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 15.72px;
+        line-height: 19px;
+        color: #292929;
+    }
+    .card-amount {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 48.8197px;
+        line-height: 58px;
+        margin-top:6px;
+        color: #0C0400;
+    }
+`
+
+
+
---
+++ src/components/posDevices/CreatePOS.js
@@ -0,0 +1,362 @@
+import { forwardRef, useEffect } from "react";
+import Dialog from "@mui/material/Dialog";
+import Slide from "@mui/material/Slide";
+import styled from "styled-components";
+import cancel from "../../assets/pos/close.svg";
+import { useState } from "react";
+import SelectCommon from "../common/SelectCommon";
+import {
+    useCreatePosMutation,
+    useGetAllMerchantsQuery,
+    useLazyGetPosQuery,
+} from "../../app/services/pos";
+import { CircularProgress } from "@mui/material";
+import SnackBar from "../common/SnackBar";
+import { lazyQueryOptions } from "../../utils/queryOptions";
+
+const Transition = forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function CreatePOS({ open, setPosDevicesModalType }) {
+    const [tab, setTab] = useState("single");
+    const [CreatePOSInput, setCreatePOSInput] = useState({
+        serialNumber: "",
+        institutionId: "",
+    });
+
+    const [snackbarInfo, setSnackbarInfo] = useState({
+        open: false,
+        message: "",
+        severity: "",
+    });
+
+    const { data: merchants } = useGetAllMerchantsQuery();
+    const [triggerGetPos] = useLazyGetPosQuery(lazyQueryOptions);
+
+    const [
+        triggerCreatePos,
+        {
+            isLoading: createPosIsLoading,
+            isSuccess: createPosIsSuccess,
+            isError: createPosIsError,
+            error: createPosError,
+        },
+    ] = useCreatePosMutation();
+
+    const merchantsNameList = merchants?.data?.reduce((acc, merchant) => {
+        acc[""] = "Select Merchant";
+        acc[merchant.id] = merchant.name;
+        return acc;
+    }, {});
+
+    const handleClose = () => {
+        setPosDevicesModalType("");
+        setCreatePOSInput({
+            serialNumber: "",
+            institutionId: "",
+        });
+    };
+
+    useEffect(() => {
+        if (createPosIsSuccess) {
+            setSnackbarInfo({
+                open: true,
+                message: "Successfully Created POS",
+                severity: "success",
+            });
+            triggerGetPos();
+            handleClose();
+        } else if (createPosIsError) {
+            const errorKey = Object.keys(createPosError?.data?.errors);
+            const errorMessage = createPosError?.data?.errors[errorKey];
+
+            setSnackbarInfo({
+                open: true,
+                message: errorMessage,
+                severity: "error",
+            });
+        }
+    }, [createPosIsSuccess, createPosIsError, createPosError]);
+
+    const handleSnackbarClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+
+        setSnackbarInfo({
+            open: false,
+            severity: snackbarInfo.severity,
+            message: snackbarInfo.message,
+        });
+    };
+
+    return (
+        <div>
+            <SnackBar
+                SnackbarMessage={snackbarInfo?.message}
+                openSnackbar={snackbarInfo.open}
+                handleClose={handleSnackbarClose}
+                snackbarSeverity={snackbarInfo.severity}
+            />
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                onClose={handleClose}
+                sx={{
+                    "& .MuiDialog-paper": {
+                        borderRadius: "8px",
+                    },
+                }}
+            >
+                <ModalContainer>
+                    <ModalHeader>
+                        <h4>Create POS</h4>
+                        <img src={cancel} alt="cancel" onClick={handleClose} />
+                    </ModalHeader>
+                    <PillsContainer>
+                        <Pills
+                            selected={tab === "single"}
+                            onClick={() => {
+                                setTab("single");
+                            }}
+                        >
+                            Single
+                        </Pills>
+                        <Pills
+                            selected={tab === "multiple"}
+                            onClick={() => {
+                                setTab("multiple");
+                            }}
+                        >
+                            Multiple
+                        </Pills>
+                    </PillsContainer>
+                    {tab === "single" ? (
+                        <ModalBody>
+                            <DesignatorInformation>
+                                <h4>POS Information</h4>
+                                <Input>
+                                    <label htmlFor="text">Serial Number</label>
+                                    <input
+                                        type={"text"}
+                                        onChange={(e) => {
+                                            setCreatePOSInput({
+                                                ...CreatePOSInput,
+                                                serialNumber: e.target.value,
+                                            });
+                                        }}
+                                        value={CreatePOSInput.serialNumber}
+                                    />
+                                </Input>
+                                <br />
+                                <br />
+                                <h4>Merchant Information</h4>
+                                <SelectCommon
+                                    value={CreatePOSInput.institutionId}
+                                    options={merchantsNameList}
+                                    onChange={(e) => {
+                                        setCreatePOSInput({
+                                            ...CreatePOSInput,
+                                            institutionId: e.target.value,
+                                        });
+                                    }}
+                                />
+                            </DesignatorInformation>
+                            <ActionButton>
+                                <CancelButton onClick={handleClose}>Cancel</CancelButton>
+                                <AcceptButton
+                                    disabled={
+                                        CreatePOSInput.serialNumber === "" ||
+                                        CreatePOSInput.institutionId === ""
+                                    }
+                                    onClick={() => {
+                                        triggerCreatePos(CreatePOSInput);
+                                    }}
+                                >
+                                    {createPosIsLoading ? (
+                                        <CircularProgress size={20} color="inherit" />
+                                    ) : (
+                                        "Create"
+                                    )}
+                                </AcceptButton>
+                            </ActionButton>
+                        </ModalBody>
+                    ) : (
+                        <ModalBody></ModalBody>
+                    )}
+                </ModalContainer>
+            </Dialog>
+        </div>
+    );
+}
+
+const ModalContainer = styled.div`
+    width: 22rem;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ModalHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 0.8rem;
+    background: #fff;
+    box-sizing: border-box;
+    position: sticky;
+    top: 0;
+    z-index: 100;
+
+    h4 {
+        font-weight: 700;
+        font-size: 1.1rem;
+        line-height: 1.2rem;
+        color: black;
+        margin-right: 5rem;
+    }
+
+    img {
+        cursor: pointer;
+        width: 1.3rem;
+    }
+`;
+
+const ModalBody = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 0.2rem 0.8rem 0.8rem 0.8rem;
+    box-sizing: border-box;
+`;
+
+const DesignatorInformation = styled.div`
+    width: 100%;
+    margin-top: 1rem;
+
+    h4 {
+        display: block;
+        font-weight: 700;
+        font-size: 0.9rem;
+        line-height: 1.1rem;
+        color: #000000;
+        margin: 0;
+        padding-bottom: 1rem;
+        width: 100%;
+    }
+`;
+
+const Input = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 100%;
+
+    label {
+        font-weight: 400;
+        font-size: 13.7255px;
+        line-height: 18px;
+        letter-spacing: 0.0571895px;
+        color: #474747;
+        margin-bottom: 0.3rem;
+    }
+
+    input {
+        width: 100%;
+        height: 48px;
+        padding: 0px 1rem;
+        background: #ffffff;
+        border: 1px solid #d3d3d3;
+        border-radius: 4.57516px;
+        box-sizing: border-box;
+        outline: none;
+    }
+
+    input:focus {
+        border: 1px solid #933d0c;
+    }
+
+    .errorMessage {
+        display: block;
+        font-size: 0.675rem;
+        margin-top: 0.1rem;
+        color: #ff0000;
+    }
+`;
+
+const ActionButton = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 2rem;
+
+    a {
+        background: #ffffff;
+        color: #933d0c;
+        border: 1px solid #933d0c;
+        height: 3rem;
+        border-radius: 4px;
+        width: ${(props) => (props.transactionStatus === "FAILED" ? "49%" : "100%")};
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        text-decoration: none;
+
+        img {
+            margin-right: 0.5rem;
+        }
+    }
+
+    button {
+        display: block;
+        border-radius: 4px;
+        height: 3rem;
+        width: 49%;
+    }
+`;
+
+const AcceptButton = styled.button`
+    background: #933d0c;
+    border: none;
+    color: #ffffff;
+
+    &:disabled {
+        background: #7a7a7a;
+    }
+`;
+
+const CancelButton = styled.button`
+    background: #ffffff;
+    color: #933d0c;
+    border: 1px solid #933d0c;
+`;
+
+const PillsContainer = styled.div`
+    display: flex;
+    align-items: baseline;
+    width: 25%;
+    justify-content: center;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    gap: 0.6rem;
+`;
+
+const Pills = styled.p`
+    cursor: pointer;
+    padding: 7.5px 18px;
+    border: none;
+    border-radius: 20px;
+    background: ${(props) => (props.selected ? "#FE6901" : "#fffff")};
+    color: ${(props) => (props.selected ? "#ffffff" : "#292929")};
+`;
