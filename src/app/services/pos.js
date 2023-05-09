447ec9558bcd3f825a860f7ef45f5cbc40ef157d
ibrahimshittu <ibshittu01@gmail.com>
2023-05-09 08:33:38 +0100

    hotfix: changed pos transaction api

--- src/app/services/pos.js
+++ src/app/services/pos.js
@@ -74,7 +74,7 @@ export const posApi = baseApi.injectEndpoints({
         }),
         getAllPosActivity: builder.query({
             query: (params) => ({
-                url: "/transaction/pos/transactions/activity",
+                url: "/transaction/pos/transaction/activity",
                 params,
             }),
         }),
@@ -101,7 +101,7 @@ export const posApi = baseApi.injectEndpoints({
         }),
         downloadPosActivityRecords: builder.query({
             query: (params) => ({
-                url: `/transaction/pos/transactions/activity/download`,
+                url: `/transaction/pos/transaction/activity/download`,
                 params,
             }),
         }),
